2606425249cca0b417c47e6af18b5add2dafcba7
korylo <waldsonkorylomachado@gmail.com>
2024-10-31 22:27:40 -0300

    alteraçao aula 13

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -28,6 +28,16 @@ tabuleiro_desenhado = False
 coordenada_x = 0
 coordenada_y = 0
 
+q1 = ''
+q2 = ''
+q3 = ''
+q4 = ''
+q5 = ''
+q6 = ''
+q7 = ''
+q8 = ''
+q9 = ''
+
 def desenha_tabuleiro(espessura,cor):
 
     
@@ -39,31 +49,46 @@ def desenha_tabuleiro(espessura,cor):
     pygame.draw.line(screen, 'yellow',(0, 200),(600, 200),espessura)
     pygame.draw.line(screen, 'yellow',(0, 400),(600, 400),espessura)
 
-def faz_jogada():    
-    if coordenada_x > 0 and coordenada_x < 200 and coordenada_y < 200:  # (x),(y)
+def faz_jogada():
+    global q1, q2, q3, q4, q5, q6, q7, q8, q9
+    status = True
+      
+    if q1 ==''and coordenada_x > 0 and coordenada_x < 200 and coordenada_y < 200:  # (x),(y)
        screen.blit(jogador_atual,(60,30)) #primario
-    elif coordenada_x >= 200 and coordenada_x < 400 and coordenada_y < 200:   
+       q1 = jogador_atual
+    elif q2 ==''and coordenada_x >= 200 and coordenada_x < 400 and coordenada_y < 200:   
         screen.blit(jogador_atual,(260,30)) #segundario
-    elif coordenada_x >= 400 and coordenada_y < 200:   
+        q2 = jogador_atual
+    elif q3 ==''and coordenada_x >= 400 and coordenada_y < 200:   
         screen.blit(jogador_atual,(460,30)) #terceiro
+        q3 = jogador_atual
 
     #segundario linha
     #
-    elif coordenada_x < 200 and coordenada_y >= 200 and coordenada_y <400:
+    elif q4 ==''and coordenada_x < 200 and coordenada_y >= 200 and coordenada_y <400:
         screen.blit(jogador_atual,(60,230)) #quarto
-    elif coordenada_x >= 200 and coordenada_x < 400 and coordenada_y >= 200 and coordenada_y < 400:   
+        q4 = jogador_atual
+    elif q5 ==''and coordenada_x >= 200 and coordenada_x < 400 and coordenada_y >= 200 and coordenada_y < 400:   
         screen.blit(jogador_atual,(260,230)) #quinto
-    elif coordenada_x >= 400 and coordenada_y >= 200 and coordenada_y < 400:     
+        q5 = jogador_atual
+    elif q6 ==''and coordenada_x >= 400 and coordenada_y >= 200 and coordenada_y < 400:     
         screen.blit(jogador_atual,(460,230)) #sexto
+        q6 = jogador_atual
 
      #terceiro linha
     #
-    elif coordenada_x < 200 and coordenada_y >= 400:
+    elif q7 ==''and coordenada_x < 200 and coordenada_y >= 400:
         screen.blit(jogador_atual,(60,430)) #setimo
-    elif coordenada_x >= 200 and coordenada_x < 400 and coordenada_y >= 400: 
+        q7 == jogador_atual
+    elif q8 ==''and coordenada_x >= 200 and coordenada_x < 400 and coordenada_y >= 400: 
         screen.blit(jogador_atual,(260,430)) #oitavo
-    elif coordenada_x >= 400 and coordenada_y >= 400:       
+        q8 == jogador_atual
+    elif q9 ==''and coordenada_x >= 400 and coordenada_y >= 400:       
         screen.blit(jogador_atual,(460,430)) #nono
+        q9 = jogador_atual
+    else:
+        status = False 
+    return status   
 
 while running: # codigo principal. 
 
@@ -81,26 +106,34 @@ while running: # codigo principal.
             print("eixo y:",click_pos[1])
             coordenada_x = click_pos[0]
             coordenada_y = click_pos[1]
-            rodadas =rodadas + 1
-            if(rodadas >= 10) : 
+            if(rodadas >= 9) : 
                 screen.fill('black')
                 rodadas = 0
                 coordenada_x = 0
                 coordenada_y = 0
                 tabuleiro_desenhado =False
             
-
-            if rodadas !=1:    
-                if(jogador_atual == personagem_x):
+            if(faz_jogada()):
+                rodadas = rodadas + 1   
+                if jogador_atual == personagem_x:
                     jogador_atual = personagem_o 
                 else:
                     jogador_atual =personagem_x 
             else:
                 jogadador_atual =personagem_x  
-            faz_jogada()
-
+        
+    
     if tabuleiro_desenhado == False:   
         desenha_tabuleiro(10,'yellow')
+        q1 = ''
+        q2 = ''
+        q3 = ''
+        q4 = ''
+        q5 = ''
+        q6 = ''
+        q7 = ''
+        q8 = ''
+        q9 = ''
         tabuleiro_desenhado = True
 
     
